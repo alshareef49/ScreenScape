264bd45a46c1d6f8cfbc80e88692674b2b86ba7b
alshareef49 <alshareef4039@gmail.com>
2023-04-24 17:15:42 +0530

    project configuration

---
+++ src/main/java/com/screenscape/ScreenScapeApplication.java
@@ -0,0 +1,13 @@
+package com.screenscape;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenScapeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenScapeApplication.class, args);
+	}
+
+}
